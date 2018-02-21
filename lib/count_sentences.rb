8cdf792df368c9dfc62788479257610473097158
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-21 21:33:11 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.last == "." ? true : false
   end
 
   def question?
-
+    self.last == "?" ? true : false
   end
 
   def exclamation?
-
+    self.last == "!" ? true : false
   end
 
   def count_sentences
